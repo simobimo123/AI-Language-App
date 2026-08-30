ad5dfeaaf4288fbea82be2310b1d0abc54134d20
simobimo123 <mohamad.simo2021@gmail.com>
2026-08-30 15:12:29 +0200

    Extract home stats API into dedicated service

--- AI_Language_App_flutter/ai_language_app_flutter/lib/services/api/api_service.dart
+++ AI_Language_App_flutter/ai_language_app_flutter/lib/services/api/api_service.dart
@@ -1,9 +1,9 @@
-import '../../core/errors/api_exception.dart';
 import '../../models/placement_models.dart';
 import 'api_client.dart';
 import 'auth_api_service.dart';
 import 'learning_api_service.dart';
 import 'placement_api_service.dart';
+import 'stats_api_service.dart';
 import 'word_api_service.dart';
 
 class ApiService {
@@ -13,6 +13,7 @@ class ApiService {
   late final AuthApiService _auth;
   late final LearningApiService _learning;
   late final PlacementApiService _placement;
+  late final StatsApiService _stats;
   late final WordApiService _words;
 
   ApiService() {
@@ -20,6 +21,7 @@ class ApiService {
     _auth = AuthApiService(_client);
     _learning = LearningApiService(_client);
     _placement = PlacementApiService(_client);
+    _stats = StatsApiService(_client);
     _words = WordApiService(_client);
   }
 
@@ -44,34 +46,21 @@ class ApiService {
     required String email,
     required String password,
   }) {
-    return _auth.register(
-      name: name,
-      email: email,
-      password: password,
-    );
+    return _auth.register(name: name, email: email, password: password);
   }
 
   Future<Map<String, dynamic>> login({
     required String email,
     required String password,
   }) {
-    return _auth.login(
-      email: email,
-      password: password,
-    );
+    return _auth.login(email: email, password: password);
   }
 
-  Future<Map<String, dynamic>> loginWithGoogle({
-    required String idToken,
-  }) {
-    return _auth.loginWithGoogle(
-      idToken: idToken,
-    );
+  Future<Map<String, dynamic>> loginWithGoogle({required String idToken}) {
+    return _auth.loginWithGoogle(idToken: idToken);
   }
 
-  Future<Map<String, dynamic>> getCurrentUser() {
-    return _auth.getCurrentUser();
-  }
+  Future<Map<String, dynamic>> getCurrentUser() => _auth.getCurrentUser();
 
   Future<Map<String, dynamic>> updateCurrentUser({
     required String name,
@@ -87,172 +76,96 @@ class ApiService {
     );
   }
 
-  Future<List<dynamic>> getLearningProfiles() {
-    return _learning.getLearningProfiles();
-  }
+  Future<List<dynamic>> getLearningProfiles() => _learning.getLearningProfiles();
 
-  Future<Map<String, dynamic>> getCurrentLearningProfile() {
-    return _learning.getCurrentLearningProfile();
-  }
+  Future<Map<String, dynamic>> getCurrentLearningProfile() =>
+      _learning.getCurrentLearningProfile();
 
   Future<Map<String, dynamic>> createLearningProfile({
     required String language,
     required String level,
-  }) {
-    return _learning.createLearningProfile(
-      language: language,
-      level: level,
-    );
-  }
+  }) =>
+      _learning.createLearningProfile(language: language, level: level);
 
   Future<Map<String, dynamic>> updateLearningProfile({
     required String language,
     required String level,
     required double progress,
-  }) {
-    return _learning.updateLearningProfile(
-      language: language,
-      level: level,
-      progress: progress,
-    );
-  }
+  }) =>
+      _learning.updateLearningProfile(
+        language: language,
+        level: level,
+        progress: progress,
+      );
 
-  Future<Map<String, dynamic>> switchLearningLanguage({
-    required String language,
-  }) {
-    return _learning.switchLearningLanguage(
-      language: language,
-    );
-  }
+  Future<Map<String, dynamic>> switchLearningLanguage({required String language}) =>
+      _learning.switchLearningLanguage(language: language);
 
-  Future<Map<String, dynamic>> getLearningPath() {
-    return _learning.getLearningPath();
-  }
+  Future<Map<String, dynamic>> getLearningPath() => _learning.getLearningPath();
 
   Future<Map<String, dynamic>> completeLesson({
     required int lessonId,
     double score = 100,
-  }) {
-    return _learning.completeLesson(
-      lessonId: lessonId,
-      score: score,
-    );
-  }
+  }) =>
+      _learning.completeLesson(lessonId: lessonId, score: score);
 
   Future<PlacementWordsResponse> getPlacementWords({
     required String language,
     required String level,
-  }) {
-    return _placement.getPlacementWords(
-      language: language,
-      level: level,
-    );
-  }
+  }) =>
+      _placement.getPlacementWords(language: language, level: level);
 
   Future<PlacementWordEvaluation> evaluatePlacementWords({
     required String language,
     required String level,
     required List<int> presentedWordIds,
     required List<int> selectedWordIds,
-  }) {
-    return _placement.evaluatePlacementWords(
-      language: language,
-      level: level,
-      presentedWordIds: presentedWordIds,
-      selectedWordIds: selectedWordIds,
-    );
-  }
+  }) =>
+      _placement.evaluatePlacementWords(
+        language: language,
+        level: level,
+        presentedWordIds: presentedWordIds,
+        selectedWordIds: selectedWordIds,
+      );
 
   Future<PlacementQuizResponse> getPlacementQuiz({
     required String language,
     required String level,
-  }) {
-    return _placement.getPlacementQuiz(
-      language: language,
-      level: level,
-    );
-  }
+  }) =>
+      _placement.getPlacementQuiz(language: language, level: level);
 
   Future<PlacementQuizEvaluation> evaluatePlacementQuiz({
     required String language,
     required String level,
     required List<Map<String, int>> answers,
-  }) {
-    return _placement.evaluatePlacementQuiz(
-      language: language,
-      level: level,
-      answers: answers,
-    );
-  }
+  }) =>
+      _placement.evaluatePlacementQuiz(
+        language: language,
+        level: level,
+        answers: answers,
+      );
 
   Future<PlacementFinalizeResponse> finalizePlacement({
     required String language,
     required String level,
-  }) {
-    return _placement.finalizePlacement(
-      language: language,
-      level: level,
-    );
-  }
+  }) =>
+      _placement.finalizePlacement(language: language, level: level);
 
   Future<Map<String, dynamic>> createWord({
     required String word,
     required String translation,
-  }) {
-    return _words.createWord(
-      word: word,
-      translation: translation,
-    );
-  }
+  }) =>
+      _words.createWord(word: word, translation: translation);
 
-  Future<List<dynamic>> getWords() {
-    return _words.getWords();
-  }
+  Future<List<dynamic>> getWords() => _words.getWords();
 
   Future<Map<String, dynamic>> updateWordStatus({
     required int wordId,
     required bool learned,
-  }) {
-    return _words.updateWordStatus(
-      wordId: wordId,
-      learned: learned,
-    );
-  }
+  }) =>
+      _words.updateWordStatus(wordId: wordId, learned: learned);
 
-  Future<void> deleteWord({
-    required int wordId,
-  }) {
-    return _words.deleteWord(
-      wordId: wordId,
-    );
-  }
+  Future<void> deleteWord({required int wordId}) => _words.deleteWord(wordId: wordId);
 
-  Future<Map<String, dynamic>> getHomeStats() async {
-    final response = await _client.get(
-      '/stats',
-      authenticated: true,
-    );
-
-    final data = _client.decodeResponse(response);
-
-    if (response.statusCode == 200) {
-      if (data is Map<String, dynamic>) {
-        return data;
-      }
-
-      if (data is Map) {
-        return Map<String, dynamic>.from(data);
-      }
-
-      throw const ApiException(
-        'Invalid response format from home stats endpoint',
-      );
-    }
-
-    throw _client.apiException(
-      data,
-      'Failed to get home statistics',
-      statusCode: response.statusCode,
-    );
-  }
+  Future<Map<String, dynamic>> getHomeStats() => _stats.getHomeStats();
 }
