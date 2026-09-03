ef58caa286a174f48eaa150984bbacc07fbc41e7
simobimo123 <mohamad.simo2021@gmail.com>
2026-09-03 18:41:36 +0200

    Expose personal sentence repository operations

--- AI_Language_App_flutter/ai_language_app_flutter/lib/repositories/word_repository.dart
+++ AI_Language_App_flutter/ai_language_app_flutter/lib/repositories/word_repository.dart
@@ -1,36 +1,40 @@
 import '../services/api/api_service.dart';
+import '../services/api/api_client.dart';
+import '../services/api/word_api_service.dart';
 
 class WordRepository {
   final ApiService apiService;
+  final WordApiService wordApiService;
 
-  WordRepository({ApiService? apiService})
-      : apiService = apiService ?? ApiService();
+  WordRepository({ApiService? apiService, WordApiService? wordApiService})
+      : apiService = apiService ?? ApiService(),
+        wordApiService = wordApiService ?? WordApiService(ApiClient());
 
-  Future<List<dynamic>> getWords() {
-    return apiService.getWords();
-  }
+  Future<List<dynamic>> getWords() => apiService.getWords();
 
   Future<Map<String, dynamic>> updateWordStatus({
     required int wordId,
     required bool learned,
-  }) {
-    return apiService.updateWordStatus(
-      wordId: wordId,
-      learned: learned,
-    );
-  }
+  }) => apiService.updateWordStatus(wordId: wordId, learned: learned);
 
-  Future<void> deleteWord({required int wordId}) {
-    return apiService.deleteWord(wordId: wordId);
-  }
+  Future<void> deleteWord({required int wordId}) => apiService.deleteWord(wordId: wordId);
 
   Future<Map<String, dynamic>> createWord({
     required String word,
     required String translation,
-  }) {
-    return apiService.createWord(
-      word: word,
-      translation: translation,
-    );
-  }
+  }) => apiService.createWord(word: word, translation: translation);
+
+  Future<Map<String, dynamic>> createSentence({
+    required String sentence,
+    required String translation,
+  }) => wordApiService.createSentence(sentence: sentence, translation: translation);
+
+  Future<List<dynamic>> getSentences() => wordApiService.getSentences();
+
+  Future<Map<String, dynamic>> updateSentenceStatus({
+    required int sentenceId,
+    required bool learned,
+  }) => wordApiService.updateSentenceStatus(sentenceId: sentenceId, learned: learned);
+
+  Future<void> deleteSentence({required int sentenceId}) => wordApiService.deleteSentence(sentenceId: sentenceId);
 }
